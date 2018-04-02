863144a2ba322ea7cd2e7814089dd1d444c03b55
Jonathan Pepin <jypepin@gmail.com>
2018-04-02 11:07:21 +0200

    allow developement to read encrypted secrets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.read_encrypted_secrets = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
