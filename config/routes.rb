b2f116563a525cf17ad923810322eedbdc24fd21
Jonathan Pepin <jypepin@gmail.com>
2018-04-04 17:14:46 +0200

    fix some buttons for clarity

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/auth/twitter/callback', to: 'accounts#create'
   post 'accounts/:id/spread_the_love', to: 'accounts#spread_the_love', as: :account_spread_the_love
   post 'accounts/:id/stop', to: 'accounts#stop', as: :account_stop
-  post 'accounts/lgout', to: 'accounts#logout', as: :account_logout
+  post 'accounts/logout', to: 'accounts#logout', as: :account_logout
 
 
   root to: 'welcome#index'
